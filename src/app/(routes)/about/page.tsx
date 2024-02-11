2c5cdb5afbc8ff7d195e454281fbbf277a8d18db
kosekijsx <leandrobordondev@gmail.com>
2024-02-11 19:47:39 -0300

    ✅update Routing & New Pages

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container/Container";
 import React from "react";
 import Link from "next/link";
 import Logo from "@/components/Logo";
+import { cardList } from "@/constants/cardlist";
 export interface Card {
   title: string;
   subtitle: string;
@@ -9,7 +10,7 @@ export interface Card {
   secondDescription?: string;
   link?: boolean;
 }
-export const Card = ({ Card }: { Card: Card }) => {
+const Card = ({ Card }: { Card: Card }) => {
   return (
     <li className="my-20">
       <h2 className="text-5xl font-black text-gray-700 dark:text-gray-300 mb-1">
@@ -34,28 +35,6 @@ export const Card = ({ Card }: { Card: Card }) => {
     </li>
   );
 };
-export const cardList = [
-  {
-    title: "What is Critikal Software?",
-    subtitle: "",
-    description: ` We&apos;re obsessive and uncompromising about the quality of our work, and that&apos;s why we&apos;ve invested heavily in recruiting and retaining the very best development talent from the UK and Europe. `,
-    secondDescription: ` 
-We believe our strength is derived from the talents, ideas, and experiences of our people, who are at the heart of everything we do here at One Beyond. We know that an inclusive workplace where all employees are valued and have the opportunity to reach their full potential is also a successful one.`,
-    link: false,
-  },
-  {
-    title: "Leandro Bordon",
-    subtitle: "CEO & Software engineer",
-    description: ` I have been working with companies for more than 4 years, building
-        high-impact applications. I have share my scrum methodologies, MVC
-        Architecture, DDD, design patterns and best practices. I&apos;m a
-        self-taught enthusiast and passionate about algorithms and functional
-        programming. I&apos;m focused on building consistent interfaces and
-        solid infrastructures, constantly seeking new challenges to push myself
-        out of the box`,
-    link: true,
-  },
-];
 
 export default function AboutMe() {
   return (
---
+++ src/constants/cardlist.ts
@@ -0,0 +1,22 @@
+export const cardList = [
+  {
+    title: "What is Critikal Software?",
+    subtitle: "",
+    description: ` We&apos;re obsessive and uncompromising about the quality of our work, and that&apos;s why we&apos;ve invested heavily in recruiting and retaining the very best development talent from the UK and Europe. `,
+    secondDescription: ` 
+We believe our strength is derived from the talents, ideas, and experiences of our people, who are at the heart of everything we do here at One Beyond. We know that an inclusive workplace where all employees are valued and have the opportunity to reach their full potential is also a successful one.`,
+    link: false,
+  },
+  {
+    title: "Leandro Bordon",
+    subtitle: "CEO & Software engineer",
+    description: ` I have been working with companies for more than 4 years, building
+        high-impact applications. I have share my scrum methodologies, MVC
+        Architecture, DDD, design patterns and best practices. I&apos;m a
+        self-taught enthusiast and passionate about algorithms and functional
+        programming. I&apos;m focused on building consistent interfaces and
+        solid infrastructures, constantly seeking new challenges to push myself
+        out of the box`,
+    link: true,
+  },
+];
